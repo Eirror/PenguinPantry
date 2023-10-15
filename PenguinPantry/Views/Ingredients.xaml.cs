f57be0ded616be2fdb4f1ba13a9c4d9d98eaaad0
Jovialist <jac@thinkman.com>
2023-10-14 22:35:39 -0700

    Some example UI
    ... that we'll have to delete at some point. My brain is so tired bro.

---
+++ PenguinPantry/Views/Ingredients.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PenguinPantry.Views;
+
+public partial class Ingredients : ContentPage
+{
+	public Ingredients()
+	{
+		InitializeComponent();
+		foo.ItemsSource = new string[] { "Milk", "Eggs", "Butter", "Highly Irradiated Elephants" };
+	}
+
+}
+
